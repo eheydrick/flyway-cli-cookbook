fb0a8dae9bdb2a66a5895c37b56ebdd518a17577
Eric Heydrick <eheydrick@gmail.com>
2016-10-05 15:28:09 -0700

    relax windows dep

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/ClouDesire/flyway-cli-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/ClouDesire/flyway-cli-cookbook/issues' if respond_to?(:issues_url)
-version          '0.3.2'
+version          '0.3.3'
 supports         'ubuntu'
 supports         'windows'
 
 depends          'ubuntu'
 depends          'java', '~> 1.17'
-depends          'windows', '~> 1.38'
+depends          'windows', '>= 1.38'
